4cf74bcac2aff001a30e862a857e878ff95f701c
Leo Tsao <leo90610@gmail.com>
2021-02-13 18:16:03 +0800

    modify final.py

--- final.py
+++ final.py
@@ -39,40 +39,40 @@ def confirm():
     intent = get_intent(sen)
 
     item, money = extract(intent, slots, sen)
-    text = label4.cget("text") + item + "\n"
-    label4.configure(text=text)
-    text = label5.cget("text") + money + "\n"
-    label5.configure(text=text)
+    text = label_item.cget("text") + item + "\n"
+    label_item.configure(text=text)
+    text = label_money.cget("text") + money + "\n"
+    label_money.configure(text=text)
 
 
 def clean():
-    label4.configure(text="")
-    label5.configure(text="")
+    label_item.configure(text="")
+    label_money.configure(text="")
 
 
 window = Tk()
 window.geometry("350x200")
 
 label1 = Label(window, text='')
-btn1 = Button(window, text="麥克風", bg="yellow", command=Voice_To_Text)
+btn_mic = Button(window, text="麥克風", bg="yellow", command=Voice_To_Text)
 entryVariable = Entry(window, text="這是文字方塊", width=30)
-btn2 = Button(window, text="確定", bg="yellow", command=confirm)
+btn_confirm = Button(window, text="確定", bg="yellow", command=confirm)
 label2 = Label(window, text='項目')
 label3 = Label(window, text='收支出  ')
 labele = Label(window, text='')
-label4 = Label(window, text='')  # item
-label5 = Label(window, text='')  # price
+label_item = Label(window, text='')  # item
+label_money = Label(window, text='')  # price
 btn3 = Button(window, text="清空", bg="yellow", command=clean)
 
 label1.grid(row=0, column=1)
-btn1.grid(row=1, column=0)
+btn_mic.grid(row=1, column=0)
 entryVariable.grid(row=1, column=1)
-btn2.grid(row=1, column=2)
+btn_confirm.grid(row=1, column=2)
 labele.grid(row=2, column=0)
 label2.grid(row=3, column=0)
 label3.grid(row=3, column=2)
-label4.grid(row=4, column=0)
-label5.grid(row=4, column=2)
+label_item.grid(row=4, column=0)
+label_money.grid(row=4, column=2)
 btn3.grid(row=1, column=3)
 # window.maxsize(400,400) #int
 window.mainloop()
